68776aecbc4968241d856b4ff92c2729d6dc9b7e
Clam <76268570+Clammerz@users.noreply.github.com>
2021-03-19 21:44:48 -0400

    Troll

--- Troll.js
+++ Troll.js
@@ -1,1 +1,4 @@
-console.log("Troll https://i.imgur.com/aSwhVbb.png");
+/* Ctrl + Shift + I -> Console -> Copy and Paste what's below ↓↓ */
+
+Troll = 'https://i.imgur.com/aSwhVbb.png'
+window.open(Troll, '_self');
